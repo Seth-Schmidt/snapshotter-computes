f90f820624fb335e7297dcf044bf6bfa260549b4
Seth <seth@elklabs.org>
2024-03-12 21:41:23 -0400

    update: snapshot processors for lite mode

--- pool_supply_volume.py
+++ pool_supply_volume.py
@@ -3,48 +3,43 @@ from typing import Dict
 from typing import Optional
 from typing import Union
 
-from redis import asyncio as aioredis
-from snapshotter.utils.callback_helpers import GenericProcessorSnapshot
+from ipfs_client.main import AsyncIPFSClient
+from snapshotter.settings.config import settings
+from snapshotter.utils.callback_helpers import GenericProcessor
 from snapshotter.utils.default_logger import logger
-from snapshotter.utils.models.message_models import PowerloomSnapshotProcessMessage
+from snapshotter.utils.models.message_models import SnapshotProcessMessage
 from snapshotter.utils.rpc import RpcHelper
 
+from .settings.config import settings as module_settings
 from .utils.core import get_asset_trade_volume
 from .utils.models.message_models import AaveSupplyVolumeSnapshot
 from .utils.models.message_models import EpochBaseSnapshot
 
 
-class AssetSupplyVolumeProcessor(GenericProcessorSnapshot):
+class AssetSupplyVolumeProcessor(GenericProcessor):
     transformation_lambdas = None
 
     def __init__(self) -> None:
         self.transformation_lambdas = []
         self._logger = logger.bind(module='AssetSupplyVolumeProcessor')
 
-    async def compute(
+    async def _compute_single(
         self,
-        epoch: PowerloomSnapshotProcessMessage,
-        redis_conn: aioredis.Redis,
+        data_source_contract_address: str,
+        min_chain_height: int,
+        max_chain_height: int,
         rpc_helper: RpcHelper,
 
     ) -> Optional[Dict[str, Union[int, float]]]:
 
-        min_chain_height = epoch.begin
-        max_chain_height = epoch.end
-
-        asset_address = epoch.data_source
-
         max_block_timestamp = int(time.time())
 
-        self._logger.debug(f'supply volume {asset_address}, computation init time {time.time()}')
         result = await get_asset_trade_volume(
-            asset_address=asset_address,
+            asset_address=data_source_contract_address,
             from_block=min_chain_height,
             to_block=max_chain_height,
-            redis_conn=redis_conn,
             rpc_helper=rpc_helper,
         )
-        self._logger.debug(f'supply volume {asset_address}, computation end time {time.time()}')
 
         if result.get('timestamp', 0) > 0:
             max_block_timestamp = result['timestamp']
@@ -55,8 +50,11 @@ class AssetSupplyVolumeProcessor(GenericProcessorSnapshot):
         events = [log for key in result.keys() for log in result[key]['logs']]
 
         supply_volume_snapshot = AaveSupplyVolumeSnapshot(
-            contract=asset_address,
-            chainHeightRange=EpochBaseSnapshot(begin=epoch.begin, end=epoch.end),
+            contract=data_source_contract_address,
+            chainHeightRange=EpochBaseSnapshot(
+                begin=min_chain_height, 
+                end=max_chain_height,
+            ),
             timestamp=max_block_timestamp,
             borrow=result['borrow']['totals'],
             repay=result['repay']['totals'],
@@ -68,3 +66,38 @@ class AssetSupplyVolumeProcessor(GenericProcessorSnapshot):
         )
 
         return supply_volume_snapshot
+    
+    def _gen_pair_idx_to_compute(self, msg_obj: SnapshotProcessMessage):
+        monitored_pairs = module_settings.initial_assets
+        current_epoch = msg_obj.epochId
+        snapshotter_hash = hash(int(settings.instance_id.lower(), 16))
+        current_day = msg_obj.day
+        return (current_epoch + snapshotter_hash + settings.slot_id + current_day) % len(monitored_pairs)
+
+    async def compute(
+        self,
+        msg_obj: SnapshotProcessMessage,
+        rpc_helper: RpcHelper,
+        anchor_rpc_helper: RpcHelper,
+        ipfs_reader: AsyncIPFSClient,
+        protocol_state_contract,
+    ):
+        min_chain_height = msg_obj.begin
+        max_chain_height = msg_obj.end
+
+        monitored_pairs = module_settings.initial_assets
+        pair_idx = self._gen_pair_idx_to_compute(msg_obj)
+        data_source_contract_address = monitored_pairs[pair_idx]
+
+        self._logger.debug(f'supply volume {data_source_contract_address} computation init time {time.time()}')
+
+        snapshot = await self._compute_single(
+            data_source_contract_address=data_source_contract_address,
+            min_chain_height=min_chain_height,
+            max_chain_height=max_chain_height,
+            rpc_helper=rpc_helper,
+        )
+
+        self._logger.debug(f'supply volume {data_source_contract_address} computation end time {time.time()}')
+
+        return [(data_source_contract_address, snapshot)]
--- pool_total_supply.py
+++ pool_total_supply.py
@@ -3,37 +3,35 @@ from typing import Dict
 from typing import Optional
 from typing import Union
 
-from redis import asyncio as aioredis
-from snapshotter.utils.callback_helpers import GenericProcessorSnapshot
+from ipfs_client.main import AsyncIPFSClient
+from snapshotter.settings.config import settings
+from snapshotter.utils.callback_helpers import GenericProcessor
 from snapshotter.utils.default_logger import logger
-from snapshotter.utils.models.message_models import PowerloomSnapshotProcessMessage
+from snapshotter.utils.models.message_models import SnapshotProcessMessage
 from snapshotter.utils.rpc import RpcHelper
 
-from .utils.core import get_asset_supply_and_debt_bulk
+from .settings.config import settings as module_settings
+from .utils.core import get_asset_supply_and_debt
 from .utils.models.data_models import AssetTotalData
 from .utils.models.message_models import AavePoolTotalAssetSnapshot
 from .utils.models.message_models import EpochBaseSnapshot
 
 
-class AssetTotalSupplyProcessor(GenericProcessorSnapshot):
+class AssetTotalSupplyProcessor(GenericProcessor):
     transformation_lambdas = None
 
     def __init__(self) -> None:
         self.transformation_lambdas = []
         self._logger = logger.bind(module='AssetTotalSupplyProcessor')
 
-    async def compute(
+    async def _compute_single(
         self,
-        epoch: PowerloomSnapshotProcessMessage,
-        redis_conn: aioredis.Redis,
+        data_source_contract_address: str,
+        min_chain_height: int,
+        max_chain_height: int,
         rpc_helper: RpcHelper,
 
     ) -> Optional[Dict[str, Union[int, float]]]:
-
-        min_chain_height = epoch.begin
-        max_chain_height = epoch.end
-
-        data_source_contract_address = epoch.data_source
 
         epoch_asset_snapshot_map_total_supply = dict()
         epoch_asset_snapshot_map_liquidity_rate = dict()
@@ -54,14 +52,11 @@ class AssetTotalSupplyProcessor(GenericProcessorSnapshot):
 
         max_block_timestamp = int(time.time())
 
-        self._logger.debug(f'asset supply {data_source_contract_address} computation init time {time.time()}')
-        asset_supply_debt_total: Dict[str, AssetTotalData] = await get_asset_supply_and_debt_bulk(
+        asset_supply_debt_total: Dict[str, AssetTotalData] = await get_asset_supply_and_debt(
             asset_address=data_source_contract_address,
             from_block=min_chain_height,
             to_block=max_chain_height,
-            redis_conn=redis_conn,
             rpc_helper=rpc_helper,
-            fetch_timestamp=True,
         )
 
         for block_num in range(min_chain_height, max_chain_height + 1):
@@ -145,6 +140,40 @@ class AssetTotalSupplyProcessor(GenericProcessorSnapshot):
                 'contract': data_source_contract_address,
             },
         )
-        self._logger.debug(f'asset supply {data_source_contract_address}, computation end time {time.time()}')
 
         return asset_total_snapshot
+    
+    def _gen_pair_idx_to_compute(self, msg_obj: SnapshotProcessMessage):
+        monitored_pairs = module_settings.initial_assets
+        current_epoch = msg_obj.epochId
+        snapshotter_hash = hash(int(settings.instance_id.lower(), 16))
+        current_day = msg_obj.day
+        return (current_epoch + snapshotter_hash + settings.slot_id + current_day) % len(monitored_pairs)
+
+    async def compute(
+        self,
+        msg_obj: SnapshotProcessMessage,
+        rpc_helper: RpcHelper,
+        anchor_rpc_helper: RpcHelper,
+        ipfs_reader: AsyncIPFSClient,
+        protocol_state_contract,
+    ):
+        min_chain_height = msg_obj.begin
+        max_chain_height = msg_obj.end
+
+        monitored_pairs = module_settings.initial_assets
+        pair_idx = self._gen_pair_idx_to_compute(msg_obj)
+        data_source_contract_address = monitored_pairs[pair_idx]
+
+        self._logger.debug(f'asset supply {data_source_contract_address} computation init time {time.time()}')
+
+        snapshot = await self._compute_single(
+            data_source_contract_address=data_source_contract_address,
+            min_chain_height=min_chain_height,
+            max_chain_height=max_chain_height,
+            rpc_helper=rpc_helper,
+        )
+
+        self._logger.debug(f'asset supply {data_source_contract_address} computation end time {time.time()}')
+
+        return [(data_source_contract_address, snapshot)]
